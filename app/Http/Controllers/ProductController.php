a934ca6965c8009b15a17761655a57939d8f2f03
Deadshot <srilankadeadshot@gmail.com>
2023-07-03 22:28:38 +0530

    Add ProductCreate and View List

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index(){
+        $products =Product::orderby('created_at')->get();
+        return view('products.index',['products' => $products]);
+    }
+    public function create(){
+        return view('products.create');
+    }
+    public function store(Request $request){
+
+        $request->validate([
+            'name'=> 'required',
+             'image' => 'required|mimes:jpg,png,svg,gif|max:2028',
+        ]);
+
+        $product = new Product;
+
+        $file_name = time().'.'.request()->image->getClientOriginalExtension();
+                                                  
+        request()->image->move(public_path('images'),$file_name);
+
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->image = $file_name;
+        $product->category = $request->category;
+        $product->quantity = $request->quantity;
+        $product->price = $request->price;
+
+        $product->save();
+
+        return redirect()->route('products.index')->with('success','Product added successfully');
+    }
 }
